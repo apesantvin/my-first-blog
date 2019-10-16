19251d2d96769b81a8925f39f13421e3ba8aa720
Joel <apalexisjoel@gmail.com>
2019-10-16 22:31:24 +0200

    Trabajo completo

--- blog/urls.py
+++ blog/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     #path('post/<int:pk>/', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     #path('post/new', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
