573da14cbd9f38244b33f22058135630f111320a
Joel <apalexisjoel@gmail.com>
2019-10-16 20:35:55 +0200

    Ultimo paso

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     #path('', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
     #path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    #path('post/new', views.post_new, name='post_new'),
 ]
