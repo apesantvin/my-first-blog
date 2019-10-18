a97ee4ce1a7fdcf1990148a2e7b8d3283033e772
Joel <apalexisjoel@gmail.com>
2019-10-18 10:54:50 +0200

    Ver autores

--- blog/views.py
+++ blog/views.py
@@ -12,7 +12,7 @@ def post_list(request):
 
 @login_required
 def post_draft_list(request):
-    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    posts = Post.objects.filter(published_date__isnull=True).filter(author=request.user).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 def post_detail(request, pk):
